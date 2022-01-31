16f747fb69a5a432fd42e80b7ea5641e70149c54
cp-radhika-s <radhika.s@canopas.com>
2022-01-31 21:42:36 +0530

    Minor changes

--- app/src/main/java/com/canopas/campose/jettaptarget/ui/theme/TapTargetView.kt
+++ app/src/main/java/com/canopas/campose/jettaptarget/ui/theme/TapTargetView.kt
@@ -3,41 +3,36 @@ package com.canopas.campose.jettaptarget.ui.theme
 import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.layout.LayoutCoordinates
+import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.boundsInRoot
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.unit.center
 import androidx.compose.ui.unit.dp
-import com.canopas.campose.jettaptarget.R
+import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 import kotlin.math.absoluteValue
 import kotlin.math.max
@@ -51,31 +46,52 @@ fun TapTarget(targets: List<TapTargetProperty> = emptyList()) {
     }
     val currentTarget = if (targets.isNotEmpty()) targets[currentTargetIndex] else null
 
-    Log.e("Recompose ", "${currentTarget} size ${targets.size}")
+    currentTarget?.let { currentTarget ->
+        val targetCoords = currentTarget.coordinates
+        val width = targetCoords.size.width
+        val height = targetCoords.size.height
 
-    currentTarget?.let {
-        val coordinates = it.coordinates
-        val width = coordinates.size.width
-        val height = coordinates.size.height
+
+        var textCoordinate: LayoutCoordinates? by remember {
+            mutableStateOf(null)
+        }
+        var txtOffsetY by remember {
+            mutableStateOf(0f)
+        }
+
         val xOffset = with(LocalDensity.current) {
-            coordinates.positionInRoot().x.toDp()
+            targetCoords.positionInRoot().x.toDp()
         }
         val yOffset = with(LocalDensity.current) {
-            coordinates.positionInRoot().y.toDp()
+            targetCoords.positionInRoot().y.toDp()
         }
 
+        val maxDimension = max(width.absoluteValue, height.absoluteValue)
+        val minDimension = min(width.absoluteValue, height.absoluteValue)
+
+        val animationSpec = infiniteRepeatable<Float>(
+            animation = tween(2500, easing = FastOutLinearInEasing),
+            repeatMode = RepeatMode.Restart,
+        )
+        var parentXOffset by remember {
+            mutableStateOf(0f)
+        }
+        var parentYOffset by remember {
+            mutableStateOf(0f)
+        }
+
+        parentXOffset = targetCoords.positionInParent().x
+        parentYOffset = targetCoords.positionInParent().y
+        Log.e(
+                        "parentXOffset",
+                        "parentXOffset: $parentXOffset parentYOffset $parentYOffset txtOffsetX $txtOffsetY"
+                    )
         Box(
-            modifier = Modifier
-                .offset(xOffset, yOffset)
+            modifier = Modifier.onGloballyPositioned {
+
+            }
         ) {
 
-            val maxDimension = max(width.absoluteValue, height.absoluteValue)
-            val minDimension = min(width.absoluteValue, height.absoluteValue)
-
-            val animationSpec = infiniteRepeatable<Float>(
-                animation = tween(2500, easing = FastOutLinearInEasing),
-                repeatMode = RepeatMode.Restart,
-            )
             val animatables = listOf(
                 remember { Animatable(0f) },
                 remember { Animatable(0f) }
@@ -91,15 +107,23 @@ fun TapTarget(targets: List<TapTargetProperty> = emptyList()) {
             }
 
             val dys = animatables.map { it.value }
-
-            Canvas(modifier = Modifier.fillMaxSize().clickable {
-                currentTargetIndex++
-            }) {
+            Canvas(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
                 drawCircle(
                     color = Purple700,
-                    center = Offset(width / 2f, height / 2f),
+                    center = Offset(parentXOffset, parentYOffset),
                     alpha = 0.8f
                 )
+            }
+
+            Canvas(modifier = Modifier
+                .fillMaxSize()
+                .offset(xOffset, yOffset)
+                .clickable {
+                    currentTargetIndex++
+                }) {
 
                 dys.forEach { dy ->
                     val minRadius = min(size.minDimension / 3f, minDimension * dy * 4f)
@@ -112,11 +136,39 @@ fun TapTarget(targets: List<TapTargetProperty> = emptyList()) {
                 }
 
                 drawCircle(
-                    color = Color.Transparent,
-                    radius = minDimension * 1.5f,
+                    color = Color.White,
+                    radius = maxDimension / 2f + 10f,
                     center = Offset(width / 2f, height / 2f)
                 )
+            }
 
+            Column(modifier = Modifier
+                .offset(y = with(LocalDensity.current) {
+                    txtOffsetY.toDp()
+                })
+                .padding(16.dp)
+                .onGloballyPositioned {
+                    textCoordinate = it
+                    val textHeight = it.size.height
+
+                    val possibleTop =
+                        targetCoords.boundsInParent().center.y - (maxDimension / 2f + 10f) - textHeight
+
+                    txtOffsetY = if (possibleTop > 0) {
+                        possibleTop
+                    } else {
+                        targetCoords.boundsInParent().center.y + (maxDimension / 2f + 10f)
+                    }
+
+
+//                    Log.e(
+//                        "possibleTop",
+//                        "Top: $possibleTop boundary $targetTop txtOffsetX $txtOffsetY"
+//                    )
+                })
+            {
+                Text(text = currentTarget.title, fontSize = 24.sp)
+                Text(text = currentTarget.subTitle, fontSize = 16.sp)
             }
 
         }
